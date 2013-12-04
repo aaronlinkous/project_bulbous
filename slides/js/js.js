4403aed3afc11e7336718e15640c5d0e06a6fc1a
Aaron Linkous <aaron@bellmobi.com>
2013-12-03 17:06:24 -0800

    basic slide layouts and keyboard listeners.

---
+++ slides/js/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+}).keydown(function(e){
+	switch(e.keyCode) {
+		case 37:
+			console.log("left");
+			break;
+		case 38:
+			console.log("up");
+			break;
+		case 39:
+			console.log("right");
+			break;
+		case 40:
+			console.log("down");
+			break;
+	}
+});
