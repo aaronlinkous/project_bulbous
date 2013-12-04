eec9833e7d544c3f6a2f02bb2a1bb3676db74f82
Aaron Linkous <aaron@bellmobi.com>
2013-12-04 13:31:45 -0800

    basic navigation

---
+++ slides/js/slides.js
@@ -0,0 +1,48 @@
+var dir;
+
+function change_slide(dir) {
+	switch(dir) {
+		case "next":
+			if(current < num) {
+				current++;
+				$(".active").removeClass("active viewed").addClass("viewed");
+				$(".slide:eq("+current+")").addClass("active");
+			}
+			break;
+		case "prev":
+			if(current > 0) {
+				current--;
+				$(".active").removeClass("active viewed");
+				$(".slide:eq("+current+")").removeClass("viewed").addClass("active");
+			}
+			break;
+		default:
+			current = dir;
+			$(".slide").removeClass("active viewed").each(function(){
+				if($(this).attr("data-slide") < dir) {
+					$(this).addClass("viewed");
+				} else if($(this).attr("data-slide") == dir) {
+					$(this).addClass("active");
+				}
+			});
+	}
+}
+
+$(document).ready(function(){
+	
+}).keydown(function(e){
+	switch(e.keyCode) {
+		case 37:
+			change_slide("prev");
+			break;
+		case 38:
+			change_slide("up");
+			break;
+		case 39:
+			change_slide("next");
+			break;
+		case 40:
+			change_slide("down");
+			break;
+	}
+});
