1fd4b263b9fad980ad83896fe57bbaaa0f97cd57
Aaron Linkous <aaron@bellmobi.com>
2013-12-06 10:38:09 -0800

    got rid of terrible JS "class" crap. Incorporated proper namespaces

--- builder/js/_home.js
+++
@@ -1,9 +0,0 @@
-var _home = Extend(CMS,{
-	name: "_home",
-	init:function(){
-		builder._init();
-	},
-	ready:function() {
-		builder._ready();
-	}
-});
--- slides/js/slides.js
+++ slides/js/slides.js
@@ -1,45 +1,43 @@
-var dir,hash,current = 0;
+(function(_slides, $, undefined ) {
+	_slides.current = 0;
 
-function change_slide(dir) {
-	console.log(dir);
-	switch(dir) {
-		case "next":
-			if(current < num) {
-				current++;
-				$(".active").removeClass("active viewed no_transition").addClass("viewed");
-				$(".slide:eq("+current+")").addClass("active");
-			}
-			break;
-		case "prev":
-			if(current > 0) {
-				current--;
-				$(".active").removeClass("active viewed no_transition");
-				$(".slide:eq("+current+")").removeClass("viewed").addClass("active no_transition");
-			}
-			break;
-		case "up":
-			break;
-		case "down":
-			break;
-		default:
-			current = dir;
-			$(".slide").removeClass("active viewed no_transition").each(function(){
-				if($(this).attr("data-slide") < dir) {
-					$(this).addClass("viewed");
-				} else if($(this).attr("data-slide") == dir) {
-					$(this).addClass("active no_transition");
+	_slides.change_slide = function(dir) {
+		console.log(dir);
+		switch(dir) {
+			case "next":
+				if(current < num) {
+					current++;
+					$(".active").removeClass("active viewed no_transition").addClass("viewed");
+					$(".slide:eq("+current+")").addClass("active");
 				}
-			});
+				break;
+			case "prev":
+				if(current > 0) {
+					current--;
+					$(".active").removeClass("active viewed no_transition");
+					$(".slide:eq("+current+")").removeClass("viewed").addClass("active no_transition");
+				}
+				break;
+			case "up":
+				break;
+			case "down":
+				break;
+			default:
+				current = dir;
+				$(".slide").removeClass("active viewed no_transition").each(function(){
+					if($(this).attr("data-slide") < dir) {
+						$(this).addClass("viewed");
+					} else if($(this).attr("data-slide") == dir) {
+						$(this).addClass("active no_transition");
+					}
+				});
+		}
+	
+		window.location.hash = "#"+current;
 	}
-
-	window.location.hash = "#"+current;
-}
+}(window._slides = window._slides || {}, jQuery));
 
 $(document).ready(function(){
-	console.log(num);
 	hash = window.location.hash;
-	if(hash){
-		hash = hash.replace("#","");
-		change_slide(hash);
-	}
+	if(hash) _slides.change_slide(hash.replace("#",""));
 });
