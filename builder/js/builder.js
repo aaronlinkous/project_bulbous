ae76e63b5a8641d952ba16cab710894eb4cbf701
Aaron Linkous <aaron@bellmobi.com>
2013-12-12 14:43:43 -0800

    built out basic styling for the slide options

--- builder/js/builder.js
+++ builder/js/builder.js
@@ -6,5 +6,28 @@
 })();
 
 (function(Builder, $, undefined ) {
+	var style_arr = {};
+
+	Builder.slide_style = function(style,val,post) {
+		styles = "";
+		style_arr[style] = val+post;
+
+		$.each(style_arr, function(style,val) {
+			styles += style+":"+val+";";
+		});
+
+		$("#slide_styles").html("#slide {"+styles+"}");
+	}
 
 }(window.Builder = window.Builder || {}, jQuery));
+
+
+$(document).ready(function(){
+	$(".slide_style").on("keyup click blur focus change paste", function(e) {
+		style = $(this).attr("data-style");
+		val = $(this).val();
+		post = $(this).attr("data-post") || "";
+
+		Builder.slide_style(style,val,post);
+	});
+});
